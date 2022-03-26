c08cc5f9e8c24775c662721a716a2acc658da5b3
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-03-26 21:49:38 +0300

    test

---
+++ data-structures-and-algorithms/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_structures_and_algorithms
+{
+    public class Linked_List
+    {
+        public Node head;
+
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public bool Include(int data)
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                if (temp.data == data)
+                {
+                    return true;
+                }
+                temp = temp.next;
+            }
+            return false;
+        }
+
+        public string To_String()
+        {
+            string outPut = "";
+            Node temp = head;
+            while (temp != null)
+            {
+                outPut += $"[{temp.data}] -> ";
+                temp = temp.next;
+            }
+            outPut += "NULL";
+            return outPut;
+        }
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+    }
