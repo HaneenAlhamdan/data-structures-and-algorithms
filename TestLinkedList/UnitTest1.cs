4f1165d31a148de685ea1f5b4610a7f0531274fd
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-03-26 22:27:18 +0300

    test6

--- TestLinkedList/UnitTest1.cs
+++ TestLinkedList/UnitTest1.cs
@@ -26,47 +26,47 @@ namespace TestLinkedList
         public void HeadTest()
         {
             Linked_List testLink = new Linked_List();
-            testLink.Insert(10);
-            testLink.Insert(20);
+            testLink.Insert(8);
+            testLink.Insert(21);
             Node temp = testLink.head;
-            Assert.Equal(20, temp.data);
+            Assert.Equal(21, temp.data);
         }
         [Fact]
         public void InsertMultipleNodesTest()
         {
             Linked_List testLink = new Linked_List();
-            testLink.Insert(9);
-            testLink.Insert(34);
-            testLink.Insert(56);
-            testLink.Insert(95);
+            testLink.Insert(4);
+            testLink.Insert(42);
+            testLink.Insert(61);
+            testLink.Insert(77);
             Node temp = testLink.head;
-            Assert.Equal(95, temp.data);
+            Assert.Equal(77, temp.data);
         }
         [Fact]
         public void IncludeFoundTest()
         {
             Linked_List testLink = new Linked_List();
-            testLink.Insert(9);
-            testLink.Insert(34);
-            Assert.True(testLink.Include(34));
+            testLink.Insert(4);
+            testLink.Insert(42);
+            Assert.True(testLink.Include(42));
         }
         [Fact]
         public void IncludeNotFoundTest()
         {
             Linked_List testLink = new Linked_List();
-            testLink.Insert(9);
-            testLink.Insert(34);
-            Assert.False(testLink.Include(100));
+            testLink.Insert(4);
+            testLink.Insert(42);
+            Assert.False(testLink.Include(125));
         }
         [Fact]
         public void ValesTest()
         {
             Linked_List testLink = new Linked_List();
-            testLink.Insert(10);
-            testLink.Insert(15);
-            testLink.Insert(18);
-            testLink.Insert(20);
-            Assert.Equal("[20] -> [18] -> [15] -> [10] -> NULL", testLink.To_String());
+            testLink.Insert(8);
+            testLink.Insert(12);
+            testLink.Insert(7);
+            testLink.Insert(21);
+            Assert.Equal("[21] -> [17] -> [12] -> [8] -> NULL", testLink.To_String());
         }
     }
 }
