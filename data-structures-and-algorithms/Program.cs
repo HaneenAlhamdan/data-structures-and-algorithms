defc4ffb2722bd857b277d63a294c9c6c274cdeb
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-03-26 21:43:29 +0300

    test

--- data-structures-and-algorithms/Program.cs
+++ data-structures-and-algorithms/Program.cs
@@ -1,98 +1,20 @@
 ﻿using System;
-
+using data_structures_and_algorithms;
 
 namespace data_structures_and_algorithms
 {
    public class Program
     {
-        public class Node
+        static void Main(string[] args)
         {
-            public String data;
-            private Node nextNode;
+            Linked_List testLink = new Linked_List();
+            testLink.Insert(10);
+            testLink.Insert(15);
+            testLink.Insert(18);
+            testLink.Insert(20);
+            Console.WriteLine(testLink.To_String());
+            Console.WriteLine(testLink.Include(10));
 
-
-            public Node(String data)
-            {
-                this.data = data;
-            }
-
-            public String getData()
-            {
-                return data;
-            }
-
-            public Node getNext()
-            {
-                return nextNode;
-            }
-
-            public void setNext(Node nextNode)
-            {
-                this.nextNode = nextNode;
-            }
         }
-
-        public class LinkedList
-        {
-            private Node head;
-            private int size = 0;
-
-
-            public void insert(String data)
-            {
-                Node node = new Node(data);
-
-                if (head == null)
-                {
-                    head = node;
-                }
-                else
-                {
-                    Node tempRef = head.getNext();
-                    node.setNext(tempRef);
-                    head.setNext(node);
-                }
-                size++;
-            }
-
-          
-            public bool includes(String data)
-            {
-                Node currentNode = head;
-                while (currentNode.getNext() != null)
-                {
-                    if (currentNode.getData() == data)
-                    {
-                        return true;
-                    }
-                    currentNode = currentNode.getNext();
-                }
-                return false;
-            }
-
-            
-           
-      public String toString()
-            {
-                String linkesListToString = "";
-
-                if (head == null)
-                {
-                    return "Empty LinkedList (*_*)";
-                }
-                else
-                {
-                    Node currentNode = head;
-                    while (currentNode != null)
-                    {
-                        linkesListToString = linkesListToString + "{ " + currentNode.getData() + " } -> ";
-                        currentNode = currentNode.getNext();
-                    }
-                    currentNode = null;
-                }
-                return linkesListToString + "NULL";
-            }
-        }
-
     }
 }
