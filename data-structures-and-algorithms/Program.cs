55f1dc20875ecb235b5e842eac604cbc0fd5c3be
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-04-07 00:45:26 +0300

    test6

--- data-structures-and-algorithms/Program.cs
+++ data-structures-and-algorithms/Program.cs
@@ -14,30 +14,30 @@ namespace data_structures_and_algorithms
          //int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
          //   Console.WriteLine(Search.Search(arr, 4));
          
-         testLink.Append(14);
-         testLink.Insert(28);
-            Console.WriteLine(testLink.To_String());
+         //testLink.Append(14);
+         //testLink.Insert(28);
+         //   Console.WriteLine(testLink.To_String());
 
-         testLink.Add_After(14, 7);
-         testLink.Add_Before(28, 50);
-            Console.WriteLine(testLink.To_String());
+         //testLink.Add_After(14, 7);
+         //testLink.Add_Before(28, 50);
+         //   Console.WriteLine(testLink.To_String());
 
-         testLink.Add_After(50, 9);
-         testLink.Append(42);
-            Console.WriteLine(testLink.To_String());
+         //testLink.Add_After(50, 9);
+         //testLink.Append(42);
+         //   Console.WriteLine(testLink.To_String());
 
-         testLink.Add_After(123, 1);
-         testLink.DeleteNode(7);
-         Console.WriteLine(testLink.To_String());
-         Console.WriteLine($"\nIs it Include 76?  {testLink.Includes(76)}");
-         Console.WriteLine($"Is it Include 50?  {testLink.Includes(50)}");
+         //testLink.Add_After(123, 1);
+         //testLink.DeleteNode(7);
+         //Console.WriteLine(testLink.To_String());
+         //Console.WriteLine($"\nIs it Include 76?  {testLink.Includes(76)}");
+         //Console.WriteLine($"Is it Include 50?  {testLink.Includes(50)}");
 
 
-            Console.WriteLine(testLink.KthForm(0));
-            Console.WriteLine(testLink.KthForm(4));
-            Console.WriteLine(testLink.KthForm(2));
-            Console.WriteLine(testLink.KthForm(23));
-            Console.WriteLine(testLink.KthForm(-5));
+         //   Console.WriteLine(testLink.KthForm(0));
+         //   Console.WriteLine(testLink.KthForm(4));
+         //   Console.WriteLine(testLink.KthForm(2));
+         //   Console.WriteLine(testLink.KthForm(23));
+         //   Console.WriteLine(testLink.KthForm(-5));
 
 
 
@@ -52,7 +52,7 @@ namespace data_structures_and_algorithms
             Console.WriteLine("List One :- " + list1.To_String());
             Console.WriteLine("List Two :- " + list2.To_String());   
             Linked_List.list_zip(list1, list2);       
-            Console.WriteLine("Zip two linked lists :- " + list1.To_String());    
+            Console.WriteLine("Merge two linked lists :- " + list1.To_String());    
 
 
 
