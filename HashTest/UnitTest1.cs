015bc3573a52fe7ed5421f81a5d0643125fa6db9
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-06-13 02:13:14 +0300

    add test

--- HashTest/UnitTest1.cs
+++ HashTest/UnitTest1.cs
@@ -20,26 +20,26 @@ namespace HashTest
             [Fact]
             public void Test2()
             {
-                string text = "Once upon a time, there was brave princess who";
+                string text = "Prepared by experienced English teachers";
                 Assert.Equal("No Repetetion", hashmapRepeatedWord.HashmapRepeatedWord(text));
             }
             [Fact]
             public void Test3()
             {
-                string text = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...";
-                Assert.Equal("it", hashmapRepeatedWord.HashmapRepeatedWord(text));
+                string text = "Prepared by experienced English teachers, the texts, articles and conversations are brief and appropriate to your level of proficiency";
+                Assert.Equal("and", hashmapRepeatedWord.HashmapRepeatedWord(text));
             }
             [Fact]
             public void Test4()
             {
-                string text = "It was a queer sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York";
-                Assert.Equal("summer", hashmapRepeatedWord.HashmapRepeatedWord(text));
+                string text = "Here you can find activities to practise your reading skills. Reading will help you to improve your understanding of the language and build your vocabulary.";
+                Assert.Equal("you", hashmapRepeatedWord.HashmapRepeatedWord(text));
             }
             [Fact]
             public void Test5()
             {
-                string text = "Once upon a time, there was a brave princess who...";
-                Assert.Equal("a", hashmapRepeatedWord.HashmapRepeatedWord(text));
+                string text = "So I know everyone's doing the right things for their the ages";
+                Assert.Equal("the", hashmapRepeatedWord.HashmapRepeatedWord(text));
             }
         }
 
